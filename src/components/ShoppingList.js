fb421e38c6b48f37caa6c783ddd305f18a452e43
AkumuOmolo <akumu.omoloh@gmail.com>
2025-07-11 22:20:49 +0300

    Adds src/components/App.js src/components/Item.js src/components/ShoppingList.js

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterBy, setFilterBy] = useState("All");
+
+  function handleChange(event) {
+    setFilterBy(event.target.value);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    return filterBy === "All" || item.category === filterBy;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +23,12 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {itemsToDisplay.map((item) => (
+          <Item
+            key={item.id}
+            name={item.name}
+            category={item.category}
+          />
         ))}
       </ul>
     </div>
